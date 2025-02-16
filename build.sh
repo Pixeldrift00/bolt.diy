e1a696a3f7c91228d82e38d9d0a426facb1f794e
Pixeldrift00 <pixeldrift00@gmail.com>
2025-02-16 07:14:07 -0800

    Update build.sh
    removed all pnpm

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Install pnpm globally
-npm install -g pnpm
-
-# Check if pnpm installation was successful
-if ! command -v pnpm &> /dev/null
-then
-    echo "pnpm could not be installed"
-    exit 1
-fi
-
 # Install dependencies
-pnpm install
+npm install
 
 # Run the build command
-pnpm run build
+npm run build
