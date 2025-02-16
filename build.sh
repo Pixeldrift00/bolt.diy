f9eb81d0b544d03ef78a59b1022c5becc15dcb3a
Pixeldrift00 <pixeldrift00@gmail.com>
2025-02-16 05:16:35 -0800

    Update build.sh
    add if not pnpm install

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 # Install pnpm
 curl -f https://get.pnpm.io/v6.16.js | node - add --global pnpm
 
+# Check if pnpm installation was successful
+if ! command -v pnpm &> /dev/null
+then
+    echo "pnpm could not be installed"
+    exit 1
+fi
+
 # Install dependencies
 pnpm install
 
